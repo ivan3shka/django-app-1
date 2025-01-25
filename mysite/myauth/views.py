6bfd09698d50d7ee2eeca8f68224970880d7fdae
ivan <iabaryshev228@hmail.com>
2025-01-25 16:10:36 -0300

    homework - 9 My Auth, login/logout

---
+++ mysite/myauth/views.py
@@ -0,0 +1,54 @@
+from http.client import responses
+
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.views import View
+
+
+# Create your views here.
+
+#def login_view(request:HttpRequest) -> HttpResponse:
+#    if request.method == 'GET':
+#        if request.user.is_authenticated:
+#            return redirect('/admin/')
+#
+#        return render(request, 'muauth/login.html')
+#
+#    username = request.POST['username']
+#    password = request.POST['password']
+#
+#    user = authenticate(request, username=username, password=password)
+#
+#    if user is not None:
+#        login(request, user)
+#        return redirect('/admin/')
+#
+#    return render(request, 'myauth/login.html',
+#                 {'error': 'Invalid login credentials'})
+
+
+def set_cookie_view(request:HttpRequest) -> HttpResponse:
+    response = HttpResponse('Cookie set ')
+    response.set_cookie('fizz', 'buzz', max_age=3600)
+    return response
+
+def get_cookie_view(request:HttpRequest) -> HttpResponse:
+    value = request.COOKIES.get('fizz', 'default value')
+    return HttpResponse(f'Cookie value: {value!r}')
+
+
+def set_session_view(request:HttpRequest) -> HttpResponse:
+    request.session['foobar'] = 'spameggs'
+    return HttpResponse('Session set!')
+
+
+def get_session_view(request:HttpRequest) -> HttpResponse:
+    value = request.session.get('foobar', 'default value')
+    return HttpResponse(f'Session value: {value!r}')
+
+class MyLogoutPage(View):
+    def get(self, request):
+        logout(request)
+        return redirect('myauth:login')
+
